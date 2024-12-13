5864324375618052df44b30dec346ab997361666
the.33 <sbenito05@gmail.com>
2024-12-13 12:16:18 +0100

    sdfsdfsdf

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -8,15 +8,32 @@ public class playerController : MonoBehaviour
     private bool isDressed;
     private bool hasEaten;
 
+    [SerializeField]
+    private GameObject bocadillo;
+
+    private playerMovement pm;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pm = GetComponent<playerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (pm.m_HorizontalInput == 0)
+        {
+            if (collision.gameObject.layer == 6 || collision.gameObject.layer == 7) bocadillo.SetActive(true);
+        }
+        else
+        {
+            bocadillo.SetActive(false);
+        }
     }
 }
